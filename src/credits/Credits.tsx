3fa98755b5994ebb1d49812d2fe176d9a0064d0d
Mikhail Chystsisakou <zoxal@pm.me>
2022-07-20 19:50:01 +0300

    initial

---
+++ src/credits/Credits.tsx
@@ -0,0 +1,6 @@
+
+const Credits = () => {
+    <p>Создано hatchery.team. Не забудьте отблагодарить создателя липенями :)</p>
+}
+
+export default Credits;
---
+++ src/legend/Legend.tsx
@@ -0,0 +1,5 @@
+const Legend = () => {
+    return <p>Легенда</p>
+}
+
+export default Legend;
