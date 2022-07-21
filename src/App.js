29d13da7aea5fbceef5cc059e7aa02277d2b06cb
Volha Lytkina <voly0621@netcracker.com>
2022-07-21 17:38:16 +0300

    add font Montserrat

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useEffect, useRef, useState } from "react";
 import { ForceGraph2D } from 'react-force-graph';
 import './App.css';
@@ -16,6 +17,14 @@ var data = {
 const pageWidth = document.documentElement.clientWidth
 const pageHeight = document.documentElement.clientHeight
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Montserrat',
+    ].join(','),
+  },
+});
+
 function App() {
   const [profileIsOpen, setProfileIsOpen] = useState(false);
   const forceRef = useRef(null);
@@ -32,23 +41,25 @@ function App() {
   const handleCloseProfile = () => setProfileIsOpen(false);
 
   return (
-    <>
-      <ForceGraph2D
-        width={pageWidth}
-        height={pageHeight}
-        graphData={data}
-        nodeLabel="id"
-        backgroundColor="#E7E7E7"
-        linkCurvature="curvature"
-        enablePointerInteraction={true}
-        linkDirectionalParticleWidth={1}
-        onNodeClick={handleNodeClick}
-        ref={forceRef}
-      />
-      {profileIsOpen &&
-        <Profile handleCloseProfile={handleCloseProfile} />
-      }
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <ForceGraph2D
+          width={pageWidth}
+          height={pageHeight}
+          graphData={data}
+          nodeLabel="id"
+          backgroundColor="#E7E7E7"
+          linkCurvature="curvature"
+          enablePointerInteraction={true}
+          linkDirectionalParticleWidth={1}
+          onNodeClick={handleNodeClick}
+          ref={forceRef}
+        />
+        {profileIsOpen &&
+          <Profile handleCloseProfile={handleCloseProfile} />
+        }
+      </>
+    </ThemeProvider>
   );
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,25 +1,45 @@
-import { Button } from '@mui/material';
-import Box from '@mui/material/Box';
+import { Button, Typography, Box } from '@mui/material';
 import React from "react";
+import './Profile.css';
 
 
-function Profile({handleCloseProfile}) {
+function Profile({ handleCloseProfile }) {
 
-    const style = {
+    const profileBoxStyle = {
         position: 'absolute',
-        top: '20%',
-        right: '20%',
+        top: '10%',
+        right: '5%',
         width: 489,
         height: 632,
         backgroundColor: "#FFFFFF",
         p: 4,
+        borderRadius: 10,
+        display: "flex",
+        flexDirection: "column",
+        gap: 5
     };
 
     return (
         <>
-            <Box sx={style}>
-                <Button onClick={handleCloseProfile} variant="text">Close</Button>
-                Hello world
+            <Box sx={profileBoxStyle}>
+                <Box className='user'>
+                    <Typography sx={{fontSize: 20, fontWeight: 600}}>Михаил Чистяков</Typography>
+                    <Typography>@zoxal</Typography>
+                    <Button onClick={handleCloseProfile} variant="text">Close</Button>
+                </Box>
+                <Box className='tags'>
+                    <Typography sx={{fontSize: 15, fontWeight: 15}}>#programmer</Typography>
+                    <Typography sx={{fontSize: 15, fontWeight: 15}}>#run</Typography>
+                    <Typography sx={{fontSize: 15, fontWeight: 15}}>#artist</Typography>
+                    <Typography sx={{fontSize: 15, fontWeight: 15}}>#extravert</Typography>
+                </Box>
+                <Box className='about'>
+                    <Typography sx={{fontSize: 20, fontWeight: 600}}>О себе</Typography>
+                    <Typography sx={{fontSize: 15, fontWeight: 400}}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing.</Typography>
+                </Box>
+                <Box className='location'>
+                    <Typography sx={{fontSize: 20, fontWeight: 600}}>Расположение</Typography>
+                </Box>
             </Box>
         </>
     );
