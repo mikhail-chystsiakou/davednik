83192bf6da0eb3f591d514feb0723083e0e665c9
Mikhail Chystsisakou <zoxal@pm.me>
2022-07-23 15:07:08 +0300

    node labels

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ import ForceGraph2D from "react-force-graph-2d"
 
 
 var data = {
-  nodes: [{ id: "Volha Lytkina", color: "#3050C1" }, { id: "B", color: "#ADA8A8" }, { id: "C", color: "#ADA8A8" }, { id: "D", color: "#ADA8A8" }],
+  nodes: [
+    { id: "Volha Lytkina", color: "#3050C1", name: "Volha" }, 
+    { id: "B", color: "#ADA8A8" }, 
+    { id: "C", color: "#ADA8A8" }, 
+    { id: "D", color: "#ADA8A8" }],
   links: [
     { source: "Volha Lytkina", target: "B", value: 8 },
     { source: "Volha Lytkina", target: "C", value: 10 },
     { source: "Volha Lytkina", target: "D", value: 6 }
   ]
 };
+
+function genRandomTree(N = 300, reverse = false) {
+  return {
+    nodes: [...Array(N).keys()].map(i => ({ id: i })),
+      links: [...Array(N).keys()]
+    .filter(id => id)
+    .map(id => ({
+      [reverse ? 'target' : 'source']: id,
+      [reverse ? 'source' : 'target']: Math.round(Math.random() * (id-1))
+    }))
+  };
+}
 
 //inner page size
 const pageWidth = document.documentElement.clientWidth
@@ -28,7 +44,7 @@ const theme = createTheme({
 });
 
 function App() {
-  const [profileIsOpen, setProfileIsOpen] = useState(true);
+  const [profileIsOpen, setProfileIsOpen] = useState(false);
   const forceRef = useRef(null);
   useEffect(() => {
     //forceRef.current.d3Force("charge").strength(-400);
@@ -37,7 +53,7 @@ function App() {
   const handleNodeClick = (node) => {
     console.log("Hello from console");
     console.log(node);
-    setProfileIsOpen(true);
+    // setProfileIsOpen(true);
   };
 
   const handleCloseProfile = () => setProfileIsOpen(false);
@@ -56,6 +72,20 @@ function App() {
           enablePointerInteraction={true}
           onNodeClick={handleNodeClick}
           ref={forceRef}
+          // d3AlphaMin={0.0228}
+          // d3AlphaDecay={0.0228}
+          // d3VelocityDecay={0.4}
+          // d3AlphaDecay={0.05}
+          nodeCanvasObjectMode={() => "after"}
+          nodeCanvasObject={(node, ctx, globalScale) => {
+            const label = node.name;
+            const fontSize = 4 ;// globalScale;
+            ctx.font = `${fontSize}px Sans-Serif`;
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = "black"; //node.color;
+            ctx.fillText(label, node.x, node.y + 8);
+          }}
         /> 
         <Search />
         {profileIsOpen &&
