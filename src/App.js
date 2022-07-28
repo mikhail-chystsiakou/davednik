7ce394891a39c61b028cfc2227fd0ce6dd4b9e9e
Mikhail Chystsisakou <zoxal@pm.me>
2022-07-28 11:44:12 +0300

    show graph only after loading

--- src/App.js
+++ src/App.js
@@ -4,19 +4,6 @@ import DavednikGraph from './components/DavednikGraph';
 import Main from './components/Main/Main';
 import LoginForm from './components/Login/LoginForm.js';
 
-var data = {
-  nodes: [
-    { id: "Volha Lytkina", color: "#3050C1", name: "Volha" },
-    { id: "B", color: "#ADA8A8" },
-    { id: "C", color: "#ADA8A8" },
-    { id: "D", color: "#ADA8A8" }],
-  links: [
-    { source: "Volha Lytkina", target: "B", value: 8 },
-    { source: "Volha Lytkina", target: "C", value: 10 },
-    { source: "Volha Lytkina", target: "D", value: 6 }
-  ]
-};
-
 const theme = createTheme({
   typography: {
     fontFamily: [
@@ -31,7 +18,7 @@ const theme = createTheme({
 });
 
 export default function App() {
-  const [graphData, setGraphData] = useState(data);
+  const [graphData, setGraphData] = useState({nodes:[], links:[]});
   const [isDialogOpen, setIsDialogOpen] = useState(true);
   const addUserNode = (user) => {
     setGraphData({
