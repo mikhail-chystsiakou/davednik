f68b6c7601f3782eea5faafc6ec8d82a9d97fe71
BigCubeCat <bigcubecat@yandex.ru>
2022-07-29 15:00:08 +0700

    add loading to About

--- src/components/Profile/About.js
+++ src/components/Profile/About.js
@@ -5,10 +5,10 @@ import {
 import TextareaAutosize from '@mui/base/TextareaAutosize';
 import { useSelector, useDispatch } from 'react-redux';
 import { getNote, updateNote } from '../../supabaseClient/api';
-
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function About({ isNotes = false, about = 'lorem ipsum', userId = "", me = "" }) {
-  const [noteText, setNoteText] = useState(about);
+  const [noteText, setNoteText] = useState(null);
   console.log(me, userId)
 
   const handleUpdate = async (value) => {
@@ -26,14 +26,14 @@ export default function About({ isNotes = false, about = 'lorem ipsum', userId =
   return (
     <Box>
       <Typography variant='h6'>{(isNotes) ? "Мои замтеки" : "О себе"}</Typography>
-      {(isNotes) ?
+      {(isNotes) ? (noteText !== null) ? 
         <TextareaAutosize
           aria-label="empty textarea"
           placeholder="Здесь вы можете оставить заметки об этом пользователе. Никто кроме вас их не увидит"
           value={noteText}
           onChange={event => handleUpdate(event.target.value)}
           style={{ minWidth: '99%' }}
-        /> :
+        /> : <CircularProgress />  :
         <Typography variant='body1'>{about}</Typography>
       }
     </Box>
